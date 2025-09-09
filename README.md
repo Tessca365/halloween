29397081ff7cfa5b9fd1eb5238a2a88d7de4f44f
Tessca365 <ttcc25@educastillalamancha.es>
2025-09-09 10:21:23 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ README.md
@@ -0,0 +1,26 @@
+ 
+
+
+> Abra esta pagina en [https://tessca365.github.io/halloween/](https://tessca365.github.io/halloween/)
+
+## Usar como extensión
+
+Este repositorio puede ser añadido como una **extensión** en MakeCode.
+
+* abra [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* haga clic en **New Project**
+* haga clic en **Extensiones** en el menú del engranaje
+* buscar **https://github.com/tessca365/halloween** e importar
+
+## Edita este proyecto
+
+Para editar este repositorio en MakeCode.
+
+* abra [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* haga clic en **Import** luego haga clic en **Import URL**
+* pegue **https://github.com/tessca365/halloween** y haga clic en importar
+
+#### Metadatos (utilizados para búsqueda, renderizado)
+
+* for PXT/arcade
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ images.g.ts
@@ -0,0 +1,44 @@
+// Código generado automáticamente. No editar.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "miImagen":return img`
+. 3 f 3 f 3 3 f f 3 f 3 3 f f . 
+f 3 3 3 3 3 3 3 3 3 3 3 3 3 3 f 
+3 3 f f f f f f f f f f f f 3 3 
+f 3 f f f f f f f f f f f f 3 f 
+f 3 5 5 5 5 f 9 9 9 f 4 4 4 3 f 
+3 3 5 f f 5 f 9 f 9 f 4 f 4 3 3 
+3 3 5 f f 5 f 9 f 9 f 4 f 4 3 f 
+f 3 5 5 5 f f 9 f 9 f 4 f 4 3 f 
+3 3 5 f f 5 f 9 f 9 f 4 f 4 3 3 
+f 3 5 f f 5 f 9 f 9 f 4 f 4 3 3 
+f 3 5 5 5 f f 9 9 9 f 4 4 4 3 f 
+3 3 f f f f f f f f f f f f 3 3 
+3 3 f f f f f f f f f f f f 3 3 
+f 3 f f f f f f f f f f f f 3 f 
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+f 3 f 3 f f 3 f f f 3 f 3 f 3 f 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Código generado automáticamente. No editar.
